b3d1bf62e7093656bfddd9f4cd6168bcc96b4a86
abolaradev <abolfazlebrahimi923395@gmail.com>
2026-08-30 14:03:46 +0330

    update config file

--- config/livewire-rate-limiter.php
+++ config/livewire-rate-limiter.php
@@ -5,7 +5,7 @@ return [
     /**
      * Maximum number of allowed attempts within the decay period.
      */
-    'maxAttempts' => 5,
+    'maxAttempts' => 10,
 
     /**
      * Number of seconds before the attempt counter is reset.
